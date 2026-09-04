5d71217a09978d2b27b90bf9c7d3debd086c9a85
Shohei.y <shohei.yokoyoko@gmail.com>
2026-09-04 12:44:50 +0900

    Generate sitemap from published indexable content

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,9 +1,15 @@
 import { getCollection } from 'astro:content';
 import { categories } from '../site-config';
+
+const indexableStaticPaths = ['/', '/categories', '/about', '/profile', '/site-map'];
+
 export async function GET({ site }: { site?: URL }) {
   const origin = site?.origin ?? 'https://replace-with-domain.example';
-  const posts = await getCollection('posts', ({ data }) => !data.draft);
-  const paths = ['/', '/categories', '/about', '/profile', '/privacy', '/disclaimer', '/contact', '/site-map', ...categories.map((c) => `/categories/${c.slug}`), ...posts.map((p) => `/articles/${p.id}`)];
+  const publishedPosts = await getCollection('posts', ({ data }) => data.draft === false);
+  const categoryPaths = categories.map((category) => `/categories/${category.slug}`);
+  const articlePaths = publishedPosts.map((post) => `/articles/${post.id}`);
+  const paths = [...indexableStaticPaths, ...categoryPaths, ...articlePaths];
   const xml = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${paths.map((path) => `<url><loc>${origin}${path}</loc></url>`).join('')}</urlset>`;
   return new Response(xml, { headers: { 'Content-Type': 'application/xml; charset=utf-8' } });
 }
+
